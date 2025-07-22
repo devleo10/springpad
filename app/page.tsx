cea27d33687911ffbbc052c361eb34bd8d4f070b
akash202004 <akashlaha48@gmail.com>
2025-07-22 17:33:51 +0530

    Add BentoPage component with detailed investment features and integrate into App

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { AiFeaturesPage } from "@/components/AiFeaturePage";
+import BentoPage from "@/components/BentoPage";
 import FeaturePage from "@/components/FeaturePage";
 import Footer from "@/components/Footer";
 import HomePage from "@/components/HomePage";
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Navbar />
       <HomePage />
+      <BentoPage />
       <FeaturePage />
       <AiFeaturesPage />
       <Footer />
