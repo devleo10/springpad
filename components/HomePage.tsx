912b042e8bcadbaedfe8c37aa0a7379ff5aaf0cd
akash202004 <akashlaha48@gmail.com>
2025-07-23 19:32:07 +0530

    feat: Update HomePage layout by adjusting margin and integrating PointerHighlight for enhanced visual appeal

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,9 +1,10 @@
 "use client";
 import React from "react";
+import { PointerHighlight } from "./ui/pointer-highlight";
 
 const HomePage = () => {
   return (
-    <div className="min-h-screen">
+    <div className="">
       <section className="bg-transparent relative overflow-hidden min-h-screen flex items-center justify-center">
         {/* Video Wrapper with relative positioning */}
         <div className="w-full max-w-5xl aspect-video overflow-hidden rounded-lg">
@@ -24,7 +25,7 @@ const HomePage = () => {
               style={{ transform: "scaley(1.1)" }}
               className="text-center text-4xl md:text-6xl lg:text-7xl font-semibold"
             >
-              <span className="bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-600 bg-clip-text text-transparent mr-[220px]">
+              <span className="bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-600 bg-clip-text text-transparent mr-[230px]">
                 {" "}
                 {/* Added mr-8 for margin-right */}
                 Smarter Funds,
@@ -32,10 +33,21 @@ const HomePage = () => {
               <span className="bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-600 bg-clip-text text-transparent">
                 Sharper Returns
               </span>
-              <span className="bg-gradient-to-b from-yellow-500 to-yellow-400 bg-clip-text text-transparent">
+              {/* <span className="bg-gradient-to-b from-yellow-500 to-yellow-400 bg-clip-text text-transparent">
                 <br />
                 with AI Precision.
-              </span>
+              </span> */}
+              <br /> 
+              <PointerHighlight
+                rectangleClassName="bg-yellow-200 border-yellow-300 leading-loose"
+                pointerClassName="text-yellow-500 h-3 w-3"
+                containerClassName="inline-block mx-1"
+              >
+                <span className="z-10 relative text-bold bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-600 bg-clip-text text-transparent">
+                  {" "}
+                  with AI Precision.
+                </span>
+              </PointerHighlight>
             </h2>
           </div>
         </div>
