eac0fd7f3e6221bf7fa11c4d19e27d5a32f8ff05
Akash <chakash793@gmail.com>
2025-07-30 12:52:27 +0530

    .

--- springbad-doc-parser/internal/service/portfolio_service.go
+++
@@ -1,8 +0,0 @@
-package service
-
-// Core business logic for portfolio processing will go here.
-
-func ProcessPortfolioFromPDF(filePath string) (interface{}, error) {
-	// TODO: Use parser to extract and process data
-	return nil, nil
-}
